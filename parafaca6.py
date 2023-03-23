9c7fc36c6b49868b571561a70329771bb9f5956c
Felipe Ricardo <85915186+felipricardo@users.noreply.github.com>
2023-03-23 19:47:07 -0300

    Add files via upload

---
+++ parafaca6.py
@@ -0,0 +1,17 @@
+"""
+6) Faça um programa que receba dez números e armazene em uma lista. Em seguida solicite um outro número e armazene em uma variável chamada multiplicador. Percorra toda a lista e multiplique cada número pelo multiplicador e apresente em tela.
+"""
+lista = [0] * 5
+lista2 = [0] * 5
+multip = 0
+
+for i in range (5):
+  print("Digite um numero: ")
+  num = int(input())
+  lista [i] = num
+print("Digite o multiplicador: ")
+multip = int(input())
+
+for i in range (5):
+  lista2 [i] = multip * lista [i]
+print("O resultado é: ", lista2)
