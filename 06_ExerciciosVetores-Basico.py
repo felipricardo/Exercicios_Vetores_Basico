98ea69bf4e73729a597c0cfbf4aa2ccd0f95891b
Felipe Ricardo <85915186+felipricardo@users.noreply.github.com>
2023-03-23 19:45:55 -0300

    Update 06_ExerciciosVetores-Basico.py

--- 06_ExerciciosVetores-Basico.py
+++ 06_ExerciciosVetores-Basico.py
@@ -92,3 +92,21 @@ for i in range (5):
       omenor = num
 print("O maior numero é o: ", omaior)
 print("O menor numero é o: ", omenor)
+
+"""
+6) Faça um programa que receba dez números e armazene em uma lista. Em seguida solicite um outro número e armazene em uma variável chamada multiplicador. Percorra toda a lista e multiplique cada número pelo multiplicador e apresente em tela.
+"""
+lista = [0] * 5
+lista2 = [0] * 5
+multip = 0
+
+for i in range (5):
+  print("Digite um numero: ")
+  num = int(input())
+  lista [i] = num
+print("Digite o multiplicador: ")
+multip = int(input())
+
+for i in range (5):
+  lista2 [i] = multip * lista [i]
+print("O resultado é: ", lista2)
