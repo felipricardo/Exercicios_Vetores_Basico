94c6f9f620e5c64e33adce39bdec9107864b9ceb
Felipe Ricardo <85915186+felipricardo@users.noreply.github.com>
2023-03-24 01:12:50 -0300

    Update parafaca7.py

--- parafaca7.py
+++ parafaca7.py
@@ -2,16 +2,16 @@
 7)	Faça um programa que receba dez números e armazene em uma lista. Em seguida copie todos os números da primeira lista para uma segunda lista, 
 mas na ordem invertida da primeira lista.
 """
-lista = [0] * 5
-inv = [0] * 5
+lista = [0] * 10
+inv = [0] * 10
 
-for i in range (5):
+for i in range (10):
   print("Digite um numero: ")
   lista [i] = int(input())
 
 #for i in range (9,-1,-1):
 #  print(lista [i])
-for i in range (5):
+for i in range (10):
   inv[i] = lista [9-i]
 print(inv)
   
