8e4da757d946797e26ee1ad1e26f42fa102f13e0
Felipe Ricardo <85915186+felipricardo@users.noreply.github.com>
2023-03-24 00:54:10 -0300

    Update parafaca6.py

--- parafaca6.py
+++ parafaca6.py
@@ -1,5 +1,6 @@
 """
-6) Faça um programa que receba dez números e armazene em uma lista. Em seguida solicite um outro número e armazene em uma variável chamada multiplicador. Percorra toda a lista e multiplique cada número pelo multiplicador e apresente em tela.
+6) Faça um programa que receba dez números e armazene em uma lista. Em seguida solicite um outro número e armazene em uma variável chamada multiplicador. 
+Percorra toda a lista e multiplique cada número pelo multiplicador e apresente em tela.
 """
 lista = [0] * 5
 lista2 = [0] * 5
